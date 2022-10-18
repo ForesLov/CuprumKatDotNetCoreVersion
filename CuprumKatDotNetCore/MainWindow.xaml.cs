1e19e9bd4aeef7982d35bad9328c0847611acee7
Fores Lov <Fores.clerc@gmail.com>
2022-10-18 16:48:05 +0300

    Добавьте файлы проекта.

---
+++ CuprumKatDotNetCore/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuprumKatDotNetCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateInventarB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InvUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateUserB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateDelB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DelivUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReportCreateForm_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateProductB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProductUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
